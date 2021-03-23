27239ab1f1a54c730236fbe932b770e1560e95e2
Silvio Traversaro <silvio@traversaro.it>
2021-03-23 14:35:34 +0100

    Do not depend on libjpeg-turbo (#1)
    * Do not depend on libjpeg-turbo
    
    The rest of conda-forge is compiled against jpeg v9, and to avoid runtime problems due to process in which both jpeg v9 and libjpeg-turbo are loaded, is better to stick to jpeg v9.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,9 @@ mkdir build && cd build
 # GNU extensions are required by Irrlicht
 CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++/-std=gnu++/g")
 
+# -fpermessive is necessary to compile irrlicht with jpeg v9
+CXXFLAGS="${CXXFLAGS} -fpermissive"
+
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
