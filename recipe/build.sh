8d0ac1801468b03a80253252a05d47033dc46263
Silvio Traversaro <silvio@traversaro.it>
2022-02-23 21:26:05 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./source/Irrlicht/jpeglib
+
 echo "This is src dir SRC_DIR"
 echo "This is CXXFLAGS: $CXXFLAGS"
 
@@ -8,7 +7,12 @@ mkdir build && cd build
 
 # Enable SDL
 sed 's/\/\/#define _IRR_COMPILE_WITH_SDL_DEVICE_/#define _IRR_COMPILE_WITH_SDL_DEVICE_/g' $SRC_DIR/include/IrrCompileConfig.h > ./PatchedIrrCompileConfig.h
-cp ./PatchedIrrCompileConfig.h $SRC_DIR/include/IrrCompileConfig.h 
+cp ./PatchedIrrCompileConfig.h $SRC_DIR/include/IrrCompileConfig.h
+
+# Avoid macOS arm64 compilation failures
+# See https://github.com/conda-forge/irrlicht-feedstock/pull/9#issuecomment-1049181315
+sed 's/(NSOpenGLPixelFormatAttribute)nil/0/g' $SRC_DIR/source/Irrlicht/MacOSX/CIrrDeviceMacOSX.mm > ./PatchedCIrrDeviceMacOSX.mm
+cp ./PatchedCIrrDeviceMacOSX.mm $SRC_DIR/source/Irrlicht/MacOSX/CIrrDeviceMacOSX.mm
 
 # GNU extensions are required by Irrlicht
 CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++/-std=gnu++/g")
